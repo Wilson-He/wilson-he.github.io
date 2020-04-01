dbc448c0f23100112b1deeee9b6b8c9667f26b50
ex_hewx <ex_hewx@partner.midea.com>
2020-04-01 14:25:56 +0800

    Site updated: 2020-04-01 14:25:52

--- js/utils.js
+++ js/utils.js
@@ -207,7 +207,7 @@ NexT.utils = {
     document.querySelectorAll('.menu-item').forEach(element => {
       var target = element.querySelector('a[href]');
       if (!target) return;
-      var isSamePath = target.pathname === location.pathname || target.pathname === location.pathname.replace('homepage.md.html', '');
+      var isSamePath = target.pathname === location.pathname || target.pathname === location.pathname.replace('index.html', '');
       var isSubPath = !CONFIG.root.startsWith(target.pathname) && location.pathname.startsWith(target.pathname);
       element.classList.toggle('menu-item-active', target.hostname === location.hostname && (isSamePath || isSubPath));
     });
